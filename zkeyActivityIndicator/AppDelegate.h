79ac6a393d0150490ae9467af86c59724b1fa1ea
zkey <mingjiameng@gmail.com>
2015-12-23 14:39:13 +0800

    Initial Commit

---
+++ zkeyActivityIndicator/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  zkeyActivityIndicator
+//
+//  Created by zkey on 12/23/15.
+//  Copyright © 2015 overcode. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ zkeyActivityIndicator/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  zkeyActivityIndicator
+//
+//  Created by zkey on 12/23/15.
+//  Copyright © 2015 overcode. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
